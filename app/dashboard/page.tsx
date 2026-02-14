c01a91c52544d88d94bdb9650188029e74a3547d
Alan Rony <alanrony25@gmail.com>
2026-02-14 00:32:29 -0300

    Protegendo dashboard

--- app/dashboard/page.tsx
+++ app/dashboard/page.tsx
@@ -1,6 +1,24 @@
 "use client"
 
+import { useEffect } from "react"
+import { useRouter } from "next/navigation"
+import { supabase } from "../lib/supabase"
+
 export default function Dashboard() {
+  const router = useRouter()
+
+  useEffect(() => {
+    const checkUser = async () => {
+      const { data } = await supabase.auth.getUser()
+
+      if (!data.user) {
+        router.push("/login")
+      }
+    }
+
+    checkUser()
+  }, [])
+
   return (
     <main className="min-h-screen bg-black text-white p-10">
 
