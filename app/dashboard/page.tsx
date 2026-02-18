0f15f2c9ce96cb8fef2382ebee48faa770b5104b
Alan Rony <alanrony25@gmail.com>
2026-02-18 11:45:28 -0300

    Projeto inicial Next.js

---
+++ app/dashboard/page.tsx
@@ -0,0 +1,7 @@
+export default function Dashboard() {
+  return (
+    <div className="flex items-center justify-center min-h-screen text-4xl text-white">
+      DASHBOARD FUNCIONANDO 🚀
+    </div>
+  )
+}
